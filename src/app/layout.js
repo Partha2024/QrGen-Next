711590a86324e1cf15fd300b10813bb1d7689813
Partha Biswas <parthabiswas2024@gmail.com>
2024-11-06 00:53:27 +0530

    added sidebar trigger

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,6 +17,7 @@ export default function RootLayout({ children }) {
                 <SidebarProvider>
                     <AppSidebar />
                     <main>
+                      <SidebarTrigger />
                         {children}
                     </main>
                     <Toaster />
