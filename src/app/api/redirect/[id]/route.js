1e5e5193896cf5322d605d807b20b6c8cd56cba1
Partha Biswas <parthabiswas2024@gmail.com>
2024-12-23 23:20:38 +0530

    added functionality to store qr scan info

--- src/app/api/redirect/[id]/route.js
+++ src/app/api/redirect/[id]/route.js
@@ -1,5 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import { NextResponse } from 'next/server';
+import geoip from 'geoip-lite';
+import useragent from 'useragent';
 
 const prisma = new PrismaClient();
 
@@ -10,9 +12,8 @@ export async function GET(req, { params }) {
         // Fetch the qr_experience type first
         const qrCode = await prisma.qRCode.findUnique({
             where: { unique_id: id },
-            select: { qr_experience: true }
+            select: { qr_experience: true, qr_code_type: true, qr_code_name: true }
         });
-
         if (!qrCode) {
             return NextResponse.json({ error: 'QR Code not found.' }, { status: 404 });
         }
@@ -45,6 +46,37 @@ export async function GET(req, { params }) {
             return NextResponse.redirect(smsLink);
         }
 
+        if(qrCode.qr_code_type === 'dynamic') {
+          const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;  
+          const geo = geoip.lookup(ip);
+
+          const scan_country = geo?.country || 'Unknown';
+          const scan_state = geo?.region || 'Unknown';
+          const scan_city = geo?.city || 'Unknown';
+
+          const userAgent = req.headers['user-agent'];
+          const agent = useragent.parse(userAgent);
+          const scan_os = agent.os.toString();
+
+          const now = new Date();
+          const scan_date = now.toISOString().split('T')[0]; // Format as YYYY-MM-DD
+          const scan_time = now.toTimeString().split(' ')[0]; // Format as HH:MM:SS
+
+          await prisma.qRScan.create({
+            data: {
+              unique_id: req.body.id || id,
+              qr_code_name: req.body.qr_code_name || qr_code_name, // Replace with appropriate data
+              scan_date: new Date(scan_date),
+              scan_time: scan_time,
+              scan_country,
+              scan_state,
+              scan_city,
+              scan_ip_address: ip,
+              scan_os,
+            },
+          });
+        }
+        res.status(200).json({ message: 'Scan recorded successfully' });
     } catch (error) {
         console.error("Error handling QR code redirect:", error);
         return NextResponse.json({ error: 'Server error' }, { status: 500 });
